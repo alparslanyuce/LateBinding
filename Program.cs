0b4d850dc5eb36516c30e430fd14bf2c7812796c
Alparslan YÜCE <44805477+alparslanyuce@users.noreply.github.com>
2021-04-18 17:41:44 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+
+namespace LateBindingIslemleri
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Assembly exec = Assembly.GetExecutingAssembly();
+            Type t = exec.GetType("LateBindingIslemleri.musteri");
+
+
+
+          object musteriInst =  Activator.CreateInstance(t);
+          MethodInfo MI = t.GetMethod("MusteriIsimGetir");
+
+            string[] parametrelerim = new string[2];
+            parametrelerim[0] = "alparslan";
+            parametrelerim[1] = "yüce";
+
+
+         string gelendeger = (string)MI.Invoke(musteriInst, parametrelerim);
+            Console.WriteLine(gelendeger);
+
+
+
+
+            Console.ReadLine();
+
+
+
+        }
+    }
+}
---
+++ musteri.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LateBindingIslemleri
+{
+   public class musteri
+    {
+        public string MusteriIsimGetir(string isim, string soyisim)
+        {
+            return $"{isim} {soyisim}";
+        }
+
+
+
+
+
+
+
+    }
+}
